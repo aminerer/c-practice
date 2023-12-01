e5b84f93578a32e2d2b0b8ff1ac995449958911f
aminerer <151168704+aminerer@users.noreply.github.com>
2023-12-01 22:07:18 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std ;
+
+main() {
+	float n,m ;
+	cin >>n >>m ;
+	cout << "plus : ";
+	cout<< n + m << '\n';
+	cout<< "minus : ";
+	cout<< n - m << '\n';
+	cout<< "divided: ";
+	cout<< n / m << '\n';
+
+	
+}
