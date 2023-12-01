13751d8b00d05d7567f8853d826e1ee802d10750
aminerer <151168704+aminerer@users.noreply.github.com>
2023-12-01 23:08:53 +0330

    Add files via upload

---
+++ main4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std ;
+
+main() {
+	char w;
+	cout << "please inter a character : " ;
+	cin >> w ;
+	switch (w) {
+		case 'A' : 
+			cout<< "VOWEL" ; 
+			break;
+		case 'a' : 
+			cout<< "VOWEL" ; 
+			break; 
+		case 'I' : 
+			cout<< "VOWEL" ; 
+			break;
+		case 'i' : 
+			cout<< "VOWEL" ; 
+			break;
+		case 'O' : 
+			cout<< "VOWEL" ; 
+			break;
+		case 'o' : 
+			cout<< "VOWEL" ; 
+			break; 
+		case 'U' : 
+			cout<< "VOWEL" ; 
+			break; 
+		case 'u' : 
+			cout<< "VOWEL" ; 
+			break;
+		case 'E' : 
+			cout<< "VOWEL" ; 
+			break; 
+		case 'e' : 
+			cout<< "VOWEL" ; 
+			break;
+	}
+		
+}
